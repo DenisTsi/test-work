70e2ee1f74661bdb8b85bb47b6ea6ddfc676f850
DenisTsi <desuta5@gmail.com>
2023-11-22 04:33:52 +0200

    Add some button and css code

--- js.js
+++ js.js
@@ -1,2 +1,11 @@
-alert('hello world')
-onclick()
+
+function sendAlert() {
+    alert('Hello!');
+}
+
+// Получаем элемент с классом "MSG"
+var myButton = document.querySelector('.MSG');
+
+// Добавляем обработчик события click
+myButton.addEventListener('click', sendAlert);
+
